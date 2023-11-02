1a092392fa3427ad7439758d1b4baa9a0eea4c53
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2023-11-02 17:51:26 +0300

    Create to do objects and date to to do

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const toDoList = document.querySelector(".todo-list");
 const addButton = document.querySelector(".add-btn");
 const textInput = document.querySelector(".text-in");
+const dateInput = document.querySelector(".date-in");
+const toDoObj = {
+  content: null,
+  date: ""
+};
+const toDoObjects = [];
 
 addButton.addEventListener("click", addItem);
 
 function addItem() {
+  toDoObj.content = textInput.value;
+  toDoObj.date = dateInput.value;
+  toDoObjects.push(toDoObj);
+
   const li = document.createElement("li");
-  li.textContent = textInput.value;
+  const liValue = document.createElement("span");
+  const liDate = document.createElement("span");
+  liValue.classList.add("todo-value");
+  liDate.classList.add("todo-date");
+  liValue.textContent = toDoObj.content;
+  liDate.textContent = toDoObj.date;
+  li.append(liValue, liDate);
   toDoList.append(li);
+  console.log(toDoObjects);
 }
